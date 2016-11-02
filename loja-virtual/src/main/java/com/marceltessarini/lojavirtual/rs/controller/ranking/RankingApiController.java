715768cb0a152458c41388df2ee21b49f6a34ee0
marceltessarini@gmail.com <marceltessarini@gmail.com>
2016-11-02 10:51:54 -0200

    Refactoring: Separando ranking de produto.

---
+++ loja-virtual/src/main/java/com/marceltessarini/lojavirtual/rs/controller/ranking/RankingApiController.java
@@ -0,0 +1,11 @@
+package com.marceltessarini.lojavirtual.rs.controller.ranking;
+
+import org.springframework.stereotype.Controller;
+
+
+@javax.annotation.Generated(value = "class io.swagger.codegen.languages.SpringCodegen", date = "2016-11-02T10:32:41.079-02:00")
+
+@Controller
+public class RankingApiController implements RankingApi {
+
+}
