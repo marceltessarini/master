badb49c240fbd56c4767a1fe9bfa80be48d4b909
marceltessarini@gmail.com <marceltessarini@gmail.com>
2017-05-20 11:29:55 -0300

    Atualizando

---
+++ loja-virtual/test/com/marceltessarini/lojavirtual/rs/HelloTest.java
@@ -0,0 +1,13 @@
+package com.marceltessarini.lojavirtual.rs;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+public class HelloTest {
+
+	// analisar
+	// http://docs.spring.io/spring-boot/docs/current/reference/html/boot-features-testing.html
+	// http://stackoverflow.com/questions/32001391/configure-specific-in-memory-database-for-testing-purpose-in-spring
+	
+}
